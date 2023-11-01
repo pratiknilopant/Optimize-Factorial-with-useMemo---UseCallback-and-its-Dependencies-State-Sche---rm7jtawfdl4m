f30de99b58965591f356f6ae4242dd01e01aed00
pratiknilopant <mr.pratiknilopant@gmail.com>
2023-11-01 17:48:17 +0530

    Final Commit

--- components/FactorialComponent.js
+++ components/FactorialComponent.js
@@ -1,13 +1,13 @@
-
+import React, { useMemo } from 'react';
 
 function factorial(n) {
   if (n <= 1) return 1;
   return n * factorial(n - 1);
 }
 
-export default function FactorialComponent() {
+export default function FactorialComponent({ number }) {
 
-
-  return <div className='result'>The factorial of {} is {}</div>;
+  const factorialResult = useMemo(() => factorial(number), [number]);
+  return <div className='result'>The factorial of {number} is {factorialResult}</div>;
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,27 @@
 import { useState } from 'react';
 import FactorialComponent from '../components/FactorialComponent';
 export default function Home() {
+
+  const [number, setNumber] = useState(0);
+
+  const handleNumberChange = (e) => {
+    setNumber(Number(e.target.value));
+  };
   
-     
   return (
     <div>
-        <h1>Welcome to Next.js!</h1>
-        <section>
-                <h2>Factorial Calculation</h2>
-                <input 
-                    type="number" 
-                    
-                   } 
-                />
-       
-                <FactorialComponent  />
-            </section>
+      <h1>Welcome to Next.js!</h1>
+      <section>
+        <h2>Factorial Calculation</h2>
+        <input
+          type="number"
+          value={number}
+          onChange={handleNumberChange}
+
+        />
+
+        <FactorialComponent number={number} />
+      </section>
     </div>
-);
+  );
 }
