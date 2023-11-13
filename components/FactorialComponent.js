7701accaf09ed6d337513974962a23f08944259f
pratiknilopant <mr.pratiknilopant@gmail.com>
2023-11-13 18:05:02 +0530

    Final Updated Commit

--- components/FactorialComponent.js
+++ components/FactorialComponent.js
@@ -1,13 +1,27 @@
-import React, { useMemo } from 'react';
+// import React, { useMemo } from 'react';
 
+// function factorial(n) {
+//   if (n <= 1) return 1;
+//   return n * factorial(n - 1);
+// }
+
+// export default function FactorialComponent({ number }) {
+
+//   const factorialResult = useMemo(() => factorial(number), [number]);
+//   return <div className='result'>The factorial of {number} is {factorialResult}</div>;
+// }
+
+
+import { useMemo } from "react";
 function factorial(n) {
   if (n <= 1) return 1;
   return n * factorial(n - 1);
 }
 
-export default function FactorialComponent({ number }) {
+export default function FactorialComponent({value}) {
 
-  const factorialResult = useMemo(() => factorial(number), [number]);
-  return <div className='result'>The factorial of {number} is {factorialResult}</div>;
+  const memoResult = useMemo(()=>{
+    return factorial(value);
+  },[value])
+  return <div className='result'>The factorial of {value} is {memoResult}</div>;
 }
-
--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,23 @@
 import { useState } from 'react';
 import FactorialComponent from '../components/FactorialComponent';
 export default function Home() {
-
-  const [number, setNumber] = useState(0);
-
-  const handleNumberChange = (e) => {
-    setNumber(Number(e.target.value));
-  };
   
+     const [value,setValue] = useState(5);
   return (
     <div>
-      <h1>Welcome to Next.js!</h1>
-      <section>
-        <h2>Factorial Calculation</h2>
-        <input
-          type="number"
-          value={number}
-          onChange={handleNumberChange}
-
-        />
-
-        <FactorialComponent number={number} />
-      </section>
+        <h1>Welcome to Next.js!</h1>
+        <section>
+                <h2>Factorial Calculation</h2>
+                <input 
+                    type="number" 
+                    value={value}
+                   onChange={(e)=>{
+                    setValue(e.target.value)
+                   }}
+                />
+       
+                <FactorialComponent  value={value}/>
+            </section>
     </div>
-  );
+);
 }
